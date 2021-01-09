5d941a80536a22689d74c913ce56583b736f898b
lester <lester@MacBook-Pro.local>
2021-01-09 18:49:26 +0800

    Site updated: 2021-01-09 18:49:26

--- js/comment.js
+++ js/comment.js
@@ -3,5 +3,5 @@ console.log('%c 欢迎来到我的博客！',
 
 console.log('%c 既然来啦，就留个言吧',
 'background-color: #49b1f5; color: #fff; padding: 8px;font-size:14px;',
-'https://shuxhan.com/message',)
+'https://lester-tai.github.io/blogs/message',)
 
